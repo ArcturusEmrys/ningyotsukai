e39fedcf434139e0636699e3f5e36e279225bd41
Arcturus Emrys <aemrys@arcturus.live>
2026-07-25 11:02:21 +0000

    Fix Linux build

--- ningyotsukai/build.rs
+++ ningyotsukai/build.rs
@@ -1,5 +1,36 @@
 use glib_build_tools;
 
+#[cfg(all(target_os = "windows", feature = "branding"))]
+fn add_windows_icon() {
+    let mut app_ico = ico::IconDir::new(ico::ResourceType::Icon);
+
+    for path in &[
+        "branding/Ningyotsukai Icon 16.png",
+        "branding/Ningyotsukai Icon 32.png",
+        "branding/Ningyotsukai Icon 64.png",
+        "branding/Ningyotsukai Icon 128.png",
+    ] {
+        println!("cargo:rerun-if-changed={}", path);
+        let file = std::fs::File::open(path).unwrap();
+        let image = ico::IconImage::read_png(file).unwrap();
+        app_ico.add_entry(ico::IconDirEntry::encode(&image).unwrap());
+    }
+
+    std::fs::create_dir_all("build").unwrap();
+
+    let ico_out = std::fs::File::create("build/Ningyotsukai.ico").unwrap();
+    app_ico.write(ico_out).unwrap();
+
+    let mut res = winresource::WindowsResource::new();
+    res.set_icon("build/Ningyotsukai.ico");
+    res.compile().unwrap();
+}
+
+#[cfg(not(all(target_os = "windows", feature = "branding")))]
+fn add_windows_icon() {
+    unreachable!();
+}
+
 fn main() {
     glib_build_tools::compile_resources(
         &["src"],
@@ -8,27 +39,6 @@ fn main() {
     );
 
     if cfg!(all(target_os = "windows", feature = "branding")) {
-        let mut app_ico = ico::IconDir::new(ico::ResourceType::Icon);
-
-        for path in &[
-            "branding/Ningyotsukai Icon 16.png",
-            "branding/Ningyotsukai Icon 32.png",
-            "branding/Ningyotsukai Icon 64.png",
-            "branding/Ningyotsukai Icon 128.png",
-        ] {
-            println!("cargo:rerun-if-changed={}", path);
-            let file = std::fs::File::open(path).unwrap();
-            let image = ico::IconImage::read_png(file).unwrap();
-            app_ico.add_entry(ico::IconDirEntry::encode(&image).unwrap());
-        }
-
-        std::fs::create_dir_all("build").unwrap();
-
-        let ico_out = std::fs::File::create("build/Ningyotsukai.ico").unwrap();
-        app_ico.write(ico_out).unwrap();
-
-        let mut res = winresource::WindowsResource::new();
-        res.set_icon("build/Ningyotsukai.ico");
-        res.compile().unwrap();
+        add_windows_icon();
     }
 }
--- puppet-inspector/build.rs
+++ puppet-inspector/build.rs
@@ -1,5 +1,36 @@
 use glib_build_tools;
 
+#[cfg(all(target_os = "windows", feature = "branding"))]
+fn add_windows_icon() {
+    let mut app_ico = ico::IconDir::new(ico::ResourceType::Icon);
+
+    for path in &[
+        "branding/Puppet Inspector Icon 16.png",
+        "branding/Puppet Inspector Icon 32.png",
+        "branding/Puppet Inspector Icon 64.png",
+        "branding/Puppet Inspector Icon 128.png",
+    ] {
+        println!("cargo:rerun-if-changed={}", path);
+        let file = std::fs::File::open(path).unwrap();
+        let image = ico::IconImage::read_png(file).unwrap();
+        app_ico.add_entry(ico::IconDirEntry::encode(&image).unwrap());
+    }
+
+    std::fs::create_dir_all("build").unwrap();
+
+    let ico_out = std::fs::File::create("build/Puppet Inspector.ico").unwrap();
+    app_ico.write(ico_out).unwrap();
+
+    let mut res = winresource::WindowsResource::new();
+    res.set_icon("build/Puppet Inspector.ico");
+    res.compile().unwrap();
+}
+
+#[cfg(not(all(target_os = "windows", feature = "branding")))]
+fn add_windows_icon() {
+    unreachable!();
+}
+
 fn main() {
     glib_build_tools::compile_resources(
         &["resources", "src"],
@@ -8,27 +39,6 @@ fn main() {
     );
 
     if cfg!(all(target_os = "windows", feature = "branding")) {
-        let mut app_ico = ico::IconDir::new(ico::ResourceType::Icon);
-
-        for path in &[
-            "branding/Puppet Inspector Icon 16.png",
-            "branding/Puppet Inspector Icon 32.png",
-            "branding/Puppet Inspector Icon 64.png",
-            "branding/Puppet Inspector Icon 128.png",
-        ] {
-            println!("cargo:rerun-if-changed={}", path);
-            let file = std::fs::File::open(path).unwrap();
-            let image = ico::IconImage::read_png(file).unwrap();
-            app_ico.add_entry(ico::IconDirEntry::encode(&image).unwrap());
-        }
-
-        std::fs::create_dir_all("build").unwrap();
-
-        let ico_out = std::fs::File::create("build/Puppet Inspector.ico").unwrap();
-        app_ico.write(ico_out).unwrap();
-
-        let mut res = winresource::WindowsResource::new();
-        res.set_icon("build/Puppet Inspector.ico");
-        res.compile().unwrap();
+        add_windows_icon();
     }
 }
